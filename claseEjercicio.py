c948e439c9a5a93aea7db4c289d8c0bd1ca821b8
SergioFasko <you@example.com>
2026-08-17 22:42:20 -0300

    Error en el css arreglado

---
+++ claseEjercicio.py
@@ -0,0 +1,22 @@
+class Animal():
+    def __init__(self, especie, nombre, sonido):
+        self.especie= especie
+        self.nombre=nombre
+        self.sonido=sonido
+
+    def que_escucha(self):
+        self.sonido= input("ingrese alguno de estos sonidos: guau, miau, kuku")
+        return self.sonido   
+
+
+    def hablar(self):
+        if self.sonido =="guau":
+            print ("Acaba de escuchar un perro")
+        elif self.sonido =="miau":
+            print ("Acaba de escuchar un gato")
+        else:
+            print ("Este es un animal desconocido")
+
+perro =Animal(perro,Colita,sonido)
+
+perro.que_escucha()
