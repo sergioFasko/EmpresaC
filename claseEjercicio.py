26f4fb0258a407a9c93ec4f7d90fdebc42f0417d
SergioFasko <you@example.com>
2026-08-18 13:33:12 -0300

    Esto agrego

--- claseEjercicio.py
+++ claseEjercicio.py
@@ -6,9 +6,9 @@ class Animal():
 
     def que_escucha(self):
         self.sonido= input("ingrese alguno de estos sonidos: guau, miau, kuku:  ")
-        return self.sonido   
+        return self.sonido
 
-
+    
     def hablar(self):
         if self.sonido =="guau":
             print ("Acaba de escuchar un perro")
@@ -17,7 +17,19 @@ class Animal():
         else:
             print ("Este es un animal desconocido")
 
+    def estatus(self):
+        print(' creo self.especie+ con nombre self.nombre, con sonido self.sonido ' )
+
 perro =Animal("perro","Colita","sonido")
 
 perro.que_escucha()
 perro.hablar()
+
+print("--------------------------------------------")
+gaton= Animal("gato","Chori","sonido")
+gaton.que_escucha()
+gaton.hablar()
+
+perro.estatus()
+
+gaton.estatus()
